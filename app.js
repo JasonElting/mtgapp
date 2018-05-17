71a15d97218500ae6c08d37edebb3e685d66526c
jason <jason.elting@gmail.com>
2018-05-17 09:44:39 -0400

    started building custom page

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs');
+var port = process.env.PORT || 3000 ;
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path')
 
-var app = http.createServer(function (req, res) {
-  /*if (req.url.indexOf('/img') != -1) {
-    var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
-      //if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      //} else {
-        //res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-        //res.write(data);
-      //}
-      res.end();
-    });
-  } else */ if (req.url.indexOf('/js') != -1) {
-    var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.write(data);
-      }
-      res.end();
-    });
-  } else if(req.url.indexOf('/css') != -1) {
-    var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-      }
-      res.end();
-    });
-  } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-      }
-      res.end();
-    });
-  }
-}).listen(port, '0.0.0.0');
+var app = express();
 
-module.exports = app;
+
+//body parser middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set static path
+
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ejs', function(req,res) {
+  res.render('index');
+
+});
+
+
+app.listen(port,function(){
+  console.log('server start');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     http = require('http'),
+//     fs = require('fs');
+
+// var app = http.createServer(function (req, res) {
+//   if (req.url.indexOf('/js') != -1) {//checks for java script and runs it
+//     var filePath = req.url.split('/js')[1];
+//     fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.write('Error 404: Resource not found.');
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, {'Content-Type': 'text/javascript'});
+//         res.write(data);
+//       }
+//       res.end();
+//     });
+//   } else if(req.url.indexOf('/css') != -1) { //checks for css and runs it
+//     var filePath = req.url.split('/css')[1];
+//     fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.write('Error 404: Resource not found.');
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, {'Content-Type': 'text/css'});
+//         res.write(data);
+//       }
+//       res.end();
+//     });
+//   }  else { //loads index.html
+//     fs.readFile(__dirname + '/public/index.html', function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.write('Error 404: Resource not found.');
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//       }
+//       res.end();
+//     });
+//   }
+// }).listen(port, '0.0.0.0');
+
+// module.exports = app;
